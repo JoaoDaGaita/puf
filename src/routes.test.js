b41529afa5826f112628ccd4e429758b6cb2278a
João Vicente <joaovsilva70@outlook.com>
2022-10-03 14:57:31 -0300

    test: change db_url var to avoid use same real db

--- src/routes.test.js
+++ src/routes.test.js
@@ -1,9 +1,15 @@
 import request from 'supertest'
+import { prisma } from '~/data'
+import bcrypt from 'bcrypt'
+
 import { app } from './server-setup'
 
 const server = app.listen()
 
 describe('User routes', () => {
+  beforeAll(async () => {
+    await prisma.User.deleteMany({})
+  })
   it('should return not found with wrong password', async () => {
     // prepare
     const email = 'joao@zam.com'
@@ -24,5 +30,40 @@ describe('User routes', () => {
 
     // expectation
     expect(result.status).toBe(404)
+  })
+  // it('should return logged user with correct credentials', async () => {
+  //   // prepare
+  //   const email = 'joao@zam.com'
+  //   const password = '123123'
+  //   // execution
+  //   const result = await request(server).get('/login').auth(email, password)
+
+  //   // expectation
+  //   expect(result.status).toBe(200)
+  //   expect(result.body).toBeTruthy()
+  //   expect(result.body.user.id).toBeTruthy()
+  //   expect(result.body.user.email).toBe(email)
+  //   //expect(result.body.user.password).toBe(false)
+  // })
+  it('should return logged user with correct credentials', async () => {
+    // prepare
+    const email = 'joao@zam.com'
+    const password = '123123'
+
+    const saltRounds = 10
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+
+    await prisma.User.create({
+      data: { email, password: hashedPassword },
+    })
+    // execution
+    const result = await request(server).get('/login').auth(email, password)
+
+    // expectation
+    expect(result.status).toBe(200)
+    expect(result.body).toBeTruthy()
+    expect(result.body.user.id).toBeTruthy()
+    expect(result.body.user.email).toBe(email)
+    //expect(result.body.user.password).toBe(false)
   })
 })
